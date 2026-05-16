4cac199aa95c35f71d631c289e467146146c5020
Terrence Scott <Terrence77477@gmail.com>
2026-05-16 15:32:32 -0500

    completed bonus features for calculator app

--- lesson2/calculator.py
+++ lesson2/calculator.py
@@ -1,3 +1,4 @@
+LANGUAGE = 'en'
 import json
 
 with open('calculator_messages.json', 'r') as file:
@@ -8,62 +9,67 @@ def prompt(msg):
 
 def invalid_number(num_str):
     try:
-        int(num_str)
+        float(num_str)
     except ValueError:
         return True
 
     return False
 
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
+
 ###############################################
 
 while True:
 
-    prompt(MESSAGES['welcome'])
+    prompt(messages('welcome'))
 
     # ask the user for the first number.
-    prompt(MESSAGES['first_number'])
+    prompt(messages('first_number'))
     num1 = input()
 
     while invalid_number(num1):
-        prompt(MESSAGES['invalid_number'])
+        prompt(messages('invalid_number'))
         num1 = input()
 
     # ask the user for the second number.
-    prompt(MESSAGES['second_number'])
+    prompt(messages('second_number'))
     num2 = input()
 
     while invalid_number(num2):
-        prompt(MESSAGES['invalid_number'])
+        prompt(messages('invalid_number'))
         num2 = input()
 
     # ask the user for an operation to perform.
     prompt(
-        f"{MESSAGES['operation']}\n"
-        f"1) {MESSAGES['add']} "
-        f"2) {MESSAGES['subtract']} "
-        f"3) {MESSAGES['multiply']} "
-        f"4) {MESSAGES['divide']}")
+        f"{messages('operation')}\n"
+        f"1) {messages('add')} "
+        f"2) {messages('subtract')} "
+        f"3) {messages('multiply')} "
+        f"4) {messages('divide')}")
     operation = input()
 
     while operation not in ['1', '2', '3', '4']:
-        prompt(f"{MESSAGES['choose']} 1, 2, 3, or 4")
+        prompt(f"{messages('choose')} 1, 2, 3, or 4")
         operation = input()
 
     # perform the operation on the two numbers.
     match operation:
         case '1':
-            output = int(num1) + int(num2)
+            output = float(num1) + float(num2)
         case '2':
-            output = int(num1) - int(num2)
+            output = float(num1) - float(num2)
         case '3':
-            output = int(num1) * int(num2)
+            output = float(num1) * float(num2)
         case '4':
-            output = int(num1) / int(num2)
+            output = float(num1) / float(num2)
+
+    output = round(output, 2) # round the result to two decimals
 
     # print the result to the terminal.
-    prompt(f"{MESSAGES['result']}: {output}")
+    prompt(f"{messages('result')}: {output}")
 
-    prompt(MESSAGES['operation_two'])
+    prompt(messages('operation_two'))
     answer = input()
     if answer and answer[0].lower() != 'y':
         break
