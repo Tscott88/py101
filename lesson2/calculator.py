f24288165bb291ab598b25bc7dfae471a5a08e33
Terrence Scott <Terrence77477@gmail.com>
2026-05-16 13:26:27 -0500

    creation and first addition of dev branch

--- lesson2/calculator.py
+++ lesson2/calculator.py
@@ -1,3 +1,8 @@
+import json
+
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
 def prompt(msg):
     print(f"==> {msg}")
 
@@ -11,43 +16,54 @@ def invalid_number(num_str):
 
 ###############################################
 
-prompt('Welcome to Calculator!')
+while True:
 
-# ask the user for the first number.
-prompt("What's the first number?")
-num1 = input()
+    prompt(MESSAGES['welcome'])
 
-while invalid_number(num1):
-    prompt("Hmm... that doesn't look like a valid number.")
+    # ask the user for the first number.
+    prompt(MESSAGES['first_number'])
     num1 = input()
 
-# ask the user for the second number.
-prompt("What's the second number?")
-num2 = input()
+    while invalid_number(num1):
+        prompt(MESSAGES['invalid_number'])
+        num1 = input()
 
-while invalid_number(num2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    # ask the user for the second number.
+    prompt(MESSAGES['second_number'])
     num2 = input()
 
-# ask the user for an operation to perform.
-prompt('What operation would you like to perform?\n \
-       1) Add 2) Subtract 3) Multiply 4) Divide')
-operation = input()
+    while invalid_number(num2):
+        prompt(MESSAGES['invalid_number'])
+        num2 = input()
 
-while operation not in ['1', '2', '3', '4']:
-    prompt('You must choose 1, 2, 3, or 4')
+    # ask the user for an operation to perform.
+    prompt(
+        f"{MESSAGES['operation']}\n"
+        f"1) {MESSAGES['add']} "
+        f"2) {MESSAGES['subtract']} "
+        f"3) {MESSAGES['multiply']} "
+        f"4) {MESSAGES['divide']}")
     operation = input()
 
-# perform the operation on the two numbers.
-match operation:
-    case '1':
-        output = int(num1) + int(num2)
-    case '2':
-        output = int(num1) - int(num2)
-    case '3':
-        output = int(num1) * int(num2)
-    case '4':
-        output = int(num1) / int(num2)
+    while operation not in ['1', '2', '3', '4']:
+        prompt(f"{MESSAGES['choose']} 1, 2, 3, or 4")
+        operation = input()
 
-# print the result to the terminal.
-prompt(f"The result is: {output}")
+    # perform the operation on the two numbers.
+    match operation:
+        case '1':
+            output = int(num1) + int(num2)
+        case '2':
+            output = int(num1) - int(num2)
+        case '3':
+            output = int(num1) * int(num2)
+        case '4':
+            output = int(num1) / int(num2)
+
+    # print the result to the terminal.
+    prompt(f"{MESSAGES['result']}: {output}")
+
+    prompt(MESSAGES['operation_two'])
+    answer = input()
+    if answer and answer[0].lower() != 'y':
+        break
